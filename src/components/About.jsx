0de90e2d8f820b5f00ab63dae9030b11990a368f
Sean Huang <seanhuangcode@gmail.com>
2025-01-31 15:10:34 -0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import recycle from '../assets/recycle.jpg'; // Ensure the path is correct
 
 const About = () => {
   return (
-    <div className='bg-neutral-100 py-24 px-10 flex flex-col md:flex-row gap-10 items-center'>
+    <div className='py-24 px-10 flex flex-col md:flex-row gap-10 items-center'>
       <div className='lg:w-[47%] bg-white shadow-xl hover:shadow-2xl rounded-xl sm:w-full flex flex-col gap-6 p-10'>
         <h1 className='text-4xl font-semibold text-emerald-500'>About Us</h1>
         <p className='text-lg text-gray-700'>
