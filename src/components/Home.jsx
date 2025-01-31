51ad25715aef6d0983f1d591931fa544bdd7c883
Hao Cui <decubingexpert@gmail.com>
2025-01-31 14:51:33 -0800

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -171,7 +171,7 @@ const Home = () => {
   };
 
   return (
-    <div className="min-h-screen relative ">
+    <div className="min-h-screen relative bg-green-200">
       <div className="container mx-auto px-4 pt-20 pb-32">
         <AnimatedBackground/>
         <div className="max-w-4xl mx-auto text-center">
